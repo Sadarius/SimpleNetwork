628fb94dbab264f85c0a5a5a067aac119e057d8f
Ugur Duman <ugurduman97@web.de>
2020-09-25 00:06:22 +0200

    Inital server

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
 
 namespace SimpleNetwork
 {
     class Program
     {
-        static void Main(string[] args)
+        private static  Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static  List<Socket> clientSockets = new List<Socket>();
+        private const int PORT = 3546;      //ports like "21", "22" are already assigned 
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("set up server");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
         }
     }
 }
